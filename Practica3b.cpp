438e51aa32505a286e39aedc0b0fafe241a4b49f
Kadaiser <dvalbuen@ucm.es>
2015-04-29 18:18:11 +0200

    Versión fina tras la 2ª practica
    Este es la solución  final revisa del la practica con modificación
    propuesta por el profesorado, incluye un anidamiento de subrutina PosMax
    dentro de DatoValido.

--- Practica3b.cpp
+++ Practica3b.cpp
@@ -10,23 +10,33 @@ int A[N]={7,3,25,4,75,2,1,1};
 int B[N];
 
 void main(){
-
-Int posmax;
-
-	for(j=0; j<N; j++){
-
-		posmax= PosMax(A,N)
-		B[j]=A[posmax];
-		A[posmax]=0;
+	int valor;
+	for(j=0, j<N,j++){
+		Valor = DatoValido(A,N);
+		if(valor != 0)	B[j]= valor;
 	}
 }
+
+int DatoValido(int A[], int N){
+	int maxValido = 0;
+	int posmx = 0;
+	
+	posmax = PosMax(A[],N);
+	if(A[posmax] > 3)	maxValido = A[posmax];
+	else maxValido = 0;
+	A[posmax]=0;
+return (maxValido);
+}
+
+
+
 --------------------------------------------------------------------------------------------------------------------------------*/
 
 		.global start
 		.EQU	N,8
 
 		.data
-A:		.word 7,3,25,4,75,2,1,1
+A:		.word 2,3,25,0,75,2,1,1
 
 		.bss
 B:		.space 4*N
@@ -45,21 +55,42 @@ for:
 		PUSH {R0, R1, R2}		//GUARDAMOS CONTENIDO DE REGSTROS
 		MOV R0, R1				//PRIMER PARAMETRO A[]
 		MOV R1, R2				//SEGUNDO PARAMETRO N
-		BL PosMax
+		BL DatoValido
 		MOV R3, R0				//GUARDAMOS EL RESULTADO DE LA SUBRRUTINA EN UNA VARIABLE AUXILIAR
 		POP {R0, R1, R2}		//RECUPERAR CONTENIDO DE LOS REGISTROS
-
-		LDR R4, [R1, R3, LSL#2]	//ALMACENO EN R5 EL VALOR MAXIMO DEL VECTOR A
-		LDR R5, =B				//DIRECCION DEL PRIMER ELEMENTO DEL VECTOR B
-		STR R4, [R5, R0, LSL#2]	//RELLENO LA POSICION DEL VETOR B CON EL SIGUIENTE ELEMENTO MAXIMO
-		MOV R4, #0				//ALMACENO EL VALOR 0
-		STR R4, [R1, R3, LSL#2]	//SUSTITULLO EL ELEMENTO CON UN 0 PARA DESCARTARLO EN EL VECTOR A PARA SUCESIVAS BUSQUEDAS
-
-		ADD R0, R0, #1			//INCREMENTO DEL CONADOR
+		LDR  R4,=B				//DIRECCION DEL VECTOR B
+		STR R3, [R4, R0, LSL#2]	//INCLUYO EL VALOR VALIDADO EN EL SEGUNDO VECTOR
+		ADD R0,R0,#1			//INCREMENTO EL CONTADOR
 		B for
 
 fin:
 		B .
+
+
+		//SUBRUTINA DatoValido PARAMETROS: R0 = A[] | R1 = N
+DatoValido:
+		PUSH {R4,R5,LR}		//PROLOGO SUBRUTINA CON ANIDAMIENTO
+
+		PUSH {R0,R1}
+		BL PosMax			//NO NECESITAMOS DECLARAR PARAMETROS YA QUE R0 Y R1 POSSEN LOS DATOS CORRESPONDIENTES
+		MOV R2, R0			//R2 SE CARGA CON EL RESULTADO DE LA SUBRUTINA
+		POP {R0,R1}
+
+		LDR R3, [R0,R2,LSL#2]
+		CMP R3, #3
+		BGT maxValido
+		MOV R4,#0		//CARGO 0 EN AXILIAR R4
+		B vaciarPos
+
+maxValido:
+		MOV R4, R3				//CARGO maxValido EN AXILIAR R4
+vaciarPos:
+		MOV R5, #0				//AUXILAR PARA COLOCAR 0 EN VECTOR A
+		STR R5, [R0,R2,LSL#2]	//SUSTITULLO LA POSICION R2 DEL VECTOR A CON UN 0
+		MOV R0, R4				//LA SUBRUTINA RETORNA EL VALOR maxValido
+		POP {R4,R5,LR}			//EPILOGO DE SUBRUTINA CON ANIDAMIENTO
+		MOV PC, LR
+
 
 
 		//SUBRUTINA PosMax	PARAMETROS: R0 = A[] | R1 = N
